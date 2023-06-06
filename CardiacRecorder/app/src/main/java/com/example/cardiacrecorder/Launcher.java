227bde0aac303de5000f2b68c1b8c665f89a8cd0
unknown <mdbappy5960@gmail.com>
2023-06-06 16:42:22 +0600

    Splash-Screen

---
+++ CardiacRecorder/app/src/main/java/com/example/cardiacrecorder/Launcher.java
@@ -0,0 +1,22 @@
+package com.example.cardiacrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Launcher.this, MainActivity.class));
+            }
+        },3000);
+    }
+}
