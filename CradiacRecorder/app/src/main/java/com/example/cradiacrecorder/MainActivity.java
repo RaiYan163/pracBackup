20113cbd22d57af5019d0d0e4340d073cfda4971
unknown <mdbappy5960@gmail.com>
2023-06-06 13:45:56 +0600

    starting

---
+++ CradiacRecorder/app/src/main/java/com/example/cradiacrecorder/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cradiacrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
